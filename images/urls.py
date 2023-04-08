7822da3a86ad9d948d3a199a98301fab19184e61
Caleb Wesley <79472476+calebjusth@users.noreply.github.com>
2023-04-08 14:25:35 +0300

    Update urls.py

--- images/urls.py
+++ images/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from django.views.generic import TemplateView
 from .views import ImageListView
 app_name = 'images'
+from .import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="images/index.html")),
     path('wallpapers/', ImageListView.as_view(), name='images'),
+    path('categories/',  views.list, name="list")
 ]
