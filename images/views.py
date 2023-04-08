cf36fe885901a654b5e260ae7fb416df7e6fb37d
Caleb Wesley <79472476+calebjusth@users.noreply.github.com>
2023-04-08 14:23:47 +0300

    Update views.py

--- images/views.py
+++ images/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import ListView
-from .models import Image
+from .models import *
+from django.shortcuts import render
 # Create your views here.
 
 class ImageListView(ListView):
     model = Image
     template_name = 'images/all_images.html'
+
+def list(request):
+    cat = Category.objects.all()
+    context = {
+        'list':cat,
+    }
+    return render(request, 'images/list.html', context)
